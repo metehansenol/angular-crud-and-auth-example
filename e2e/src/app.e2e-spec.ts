d311e9f391f3bdf1706d2f9b003657038a58b36a
Metehan Senol <msenol@lely.com>
2018-10-01 17:28:47 +0300

    app e2e tests added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app navbar', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to webtrekk-js-developer-challenge!');
+    expect(page.getAppNavbarTitle()).toEqual('Webtrekk JS Developer Challenge');
   });
 });
