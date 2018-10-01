795d20d0ee2a03ea4c4404be773664ddd63b664c
Metehan Senol <msenol@lely.com>
2018-10-01 16:21:31 +0300

    app component unit tests added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,38 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'webtrekk-js-developer-challenge'`, async(() => {
+
+  it(`should have as navbar title 'Webtrekk JS Developer Challenge'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('webtrekk-js-developer-challenge');
+    expect(app.title).toEqual('Webtrekk JS Developer Challenge');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render app navbar', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to webtrekk-js-developer-challenge!');
+    expect(compiled.querySelector('app-navbar')).not.toBe(null);
   }));
 });
