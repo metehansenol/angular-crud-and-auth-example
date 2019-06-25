e3ff044089dd788a81be494944671a0b028c11bf
metehansenol <metehansenol@gmail.com>
2019-06-25 12:28:30 +0300

    navbar title updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Webtrekk JS Developer Challenge'
+  title = 'Angular Customers App'
 }
